b3214ad00b3ae544ef0fa8bd685c30405e7f7320
Andrew Piper <andrew@threax.com>
2021-05-13 15:11:53 -0400

    Added push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/Threax.ProcessHelper.git
+git push https://github.com/threax/Threax.ProcessHelper.git --tags
+git push https://gitlab.com/threax-software/Threax.ProcessHelper.git
+git push https://gitlab.com/threax-software/Threax.ProcessHelper.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.ProcessHelper
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.ProcessHelper --tags
