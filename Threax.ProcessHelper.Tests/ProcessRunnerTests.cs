388f2dbe044fb21fbba251bd273d7d8ec1571bcf
Andrew Piper <andrew@threax.com>
2021-11-14 05:50:50 -0500

    Added some helper extension methods for the process helper.

--- Threax.ProcessHelper.Tests/ProcessRunnerTests.cs
+++ Threax.ProcessHelper.Tests/ProcessRunnerTests.cs
@@ -1,9 +1,6 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Moq;
 using System;
 using System.Diagnostics;
-using Threax.AspNetCore.Tests;
+using System.Text.Json;
 using Xunit;
 
 namespace Threax.ProcessHelper.Tests
@@ -40,6 +37,82 @@ namespace Threax.ProcessHelper.Tests
             var startInfo = new ProcessStartInfo("pwsh", "asdfdsaf");
             var result = processRunner.Run(startInfo);
             Assert.Equal(64, result);
+        }
+
+        [Fact]
+        public void JsonProcess()
+        {
+            var processRunner = new ProcessRunner();
+            var jsonObj = new { hello = "hi" };
+            var startInfo = new ProcessStartInfo("pwsh") { ArgumentList = { "-c", $"'{JsonSerializer.Serialize(jsonObj)}'" } };
+            var result = processRunner.RunJsonProcess(startInfo, "could not parse json result");
+            Assert.Equal("hi", result["hello"]);
+        }
+
+        [Fact]
+        public void JsonProcessFail()
+        {
+            var processRunner = new ProcessRunner();
+            var startInfo = new ProcessStartInfo("pwsh") { ArgumentList = { "-c", $"'broken'" } };
+            Assert.ThrowsAny<Exception>(() => processRunner.RunJsonProcess(startInfo, "could not parse json result"));
+        }
+
+        class TestType
+        {
+            public String Hello { get; set; } = "hi";
+        }
+
+        [Fact]
+        public void JsonProcessTyped()
+        {
+            var processRunner = new ProcessRunner();
+            var jsonObj = new TestType();
+            var startInfo = new ProcessStartInfo("pwsh") { ArgumentList = { "-c", $"'{JsonSerializer.Serialize(jsonObj)}'" } };
+            var result = processRunner.RunJsonProcess<TestType>(startInfo, "could not parse json result");
+            Assert.Equal("hi", result.Hello);
+        }
+
+        [Fact]
+        public void JsonProcessTypedFail()
+        {
+            var processRunner = new ProcessRunner();
+            var jsonObj = new TestType();
+            var startInfo = new ProcessStartInfo("pwsh") { ArgumentList = { "-c", $"'broken'" } };
+            Assert.ThrowsAny<Exception>(() => processRunner.RunJsonProcess<TestType>(startInfo, "could not parse json result"));
+        }
+
+        [Fact]
+        public void StringProcess()
+        {
+            var processRunner = new ProcessRunner();
+            var startInfo = new ProcessStartInfo("pwsh") { ArgumentList = { "-c", "'hi'" } };
+            var result = processRunner.RunStringProcess(startInfo, "could not run string process");
+            Assert.Equal("hi", result);
+        }
+
+        [Fact]
+        public void StringProcessFail()
+        {
+            var processRunner = new ProcessRunner();
+            var startInfo = new ProcessStartInfo("pwsh") { ArgumentList = { "-thisisnotavalidarg", "'hi'" } };
+            Assert.ThrowsAny<Exception>(() => processRunner.RunStringProcess(startInfo, "could not run string process"));
+        }
+
+        [Fact]
+        public void StringProcessAddNewlines()
+        {
+            var processRunner = new ProcessRunner();
+            var startInfo = new ProcessStartInfo("pwsh") { ArgumentList = { "-c", "'hi'" } };
+            var result = processRunner.RunStringProcessAddNewlines(startInfo, "could not run string process");
+            Assert.Equal("hi" + Environment.NewLine, result);
+        }
+
+        [Fact]
+        public void StringProcessAddNewlinesFail()
+        {
+            var processRunner = new ProcessRunner();
+            var startInfo = new ProcessStartInfo("pwsh") { ArgumentList = { "-thisisnotavalidarg", "'hi'" } };
+            Assert.ThrowsAny<Exception>(() => processRunner.RunStringProcessAddNewlines(startInfo, "could not run string process"));
         }
     }
 }
---
+++ Threax.ProcessHelper/ProcessRunnerExtensions.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Threax.ProcessHelper
+{
+    public static class ProcessRunnerExtensions
+    {
+        public static JToken RunJsonProcess(this IProcessRunner processRunner, ProcessStartInfo startInfo, String errorMessage, int validExitCode = 0)
+        {
+            return RunJsonProcess(processRunner, startInfo, r => r != validExitCode ? errorMessage : null);
+        }
+
+        public static JToken RunJsonProcess(this IProcessRunner processRunner, ProcessStartInfo startInfo, Func<int, string?> handleExitCode)
+        {
+            var jsonRunner = new JsonOutputProcessRunner(processRunner, true);
+            jsonRunner.Run(startInfo);
+
+            var error = handleExitCode(jsonRunner.LastExitCode);
+            if (error != null)
+            {
+                throw new InvalidOperationException($"{error}. Got exit code '{jsonRunner.LastExitCode}'.");
+            }
+
+            return jsonRunner.GetResult();
+        }
+
+        public static T? RunJsonProcess<T>(this IProcessRunner processRunner, ProcessStartInfo startInfo, String errorMessage, int validExitCode = 0)
+        {
+            return RunJsonProcess<T>(processRunner, startInfo, r => r != validExitCode ? errorMessage : null);
+        }
+
+        public static T? RunJsonProcess<T>(this IProcessRunner processRunner, ProcessStartInfo startInfo, Func<int, string?> handleExitCode)
+        {
+            var jsonRunner = new JsonOutputProcessRunner(processRunner, true);
+            jsonRunner.Run(startInfo);
+
+            var error = handleExitCode(jsonRunner.LastExitCode);
+            if (error != null)
+            {
+                throw new InvalidOperationException($"{error}. Got exit code '{jsonRunner.LastExitCode}'.");
+            }
+
+            return jsonRunner.GetResult<T>();
+        }
+
+        public static String RunStringProcess(this IProcessRunner processRunner, ProcessStartInfo startInfo, String errorMessage, int validExitCode = 0)
+        {
+            return RunStringProcess(processRunner, startInfo, r => r != validExitCode ? errorMessage : null);
+        }
+
+        public static String RunStringProcess(this IProcessRunner processRunner, ProcessStartInfo startInfo, Func<int, string?> handleExitCode)
+        {
+            var sb = new StringBuilder();
+            var exit = processRunner.Run(startInfo, new ProcessEvents()
+            {
+                OutputDataReceived = (o, i) =>
+                {
+                    i.AllowOutput = false;
+                    sb.Append(i.DataReceivedEventArgs.Data);
+                }
+            });
+
+            var error = handleExitCode(exit);
+            if (error != null)
+            {
+                throw new InvalidOperationException($"{error}. Got exit code '{exit}'.");
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Run a string process using a stringbuilder to process each line of output.
+        /// </summary>
+        /// <returns></returns>
+        public static String RunStringProcessAddNewlines(this IProcessRunner processRunner, ProcessStartInfo startInfo, String errorMessage, int validExitCode = 0)
+        {
+            return RunStringProcessAddNewlines(processRunner, startInfo, r => r != validExitCode ? errorMessage : null);
+        }
+
+        /// <summary>
+        /// Run a string process using a stringbuilder to process each line of output.
+        /// </summary>
+        /// <returns></returns>
+        public static String RunStringProcessAddNewlines(this IProcessRunner processRunner, ProcessStartInfo startInfo, Func<int, string?> handleExitCode)
+        {
+            var sb = new StringBuilder();
+            var exit = processRunner.Run(startInfo, new ProcessEvents()
+            {
+                OutputDataReceived = (o, i) =>
+                {
+                    i.AllowOutput = false;
+                    if (i.DataReceivedEventArgs.Data != null)
+                    {
+                        sb.AppendLine(i.DataReceivedEventArgs.Data);
+                    }
+                }
+            });
+
+            var error = handleExitCode(exit);
+            if (error != null)
+            {
+                throw new InvalidOperationException($"{error}. Got exit code '{exit}'.");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
