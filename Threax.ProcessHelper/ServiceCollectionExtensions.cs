452a038771e205c6399ac875359603f43aa190e4
Andrew Piper <andrew.piper@agilethought.com>
2022-11-28 16:08:21 -0500

    Make it possible to change the log type.

--- Threax.ProcessHelper/ServiceCollectionExtensions.cs
+++ Threax.ProcessHelper/ServiceCollectionExtensions.cs
@@ -10,24 +10,29 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static IServiceCollection AddThreaxProcessHelper(this IServiceCollection services, Action<ThreaxProcessHelperOptions>? configure = null)
         {
+            return AddThreaxProcessHelper<DefaultLog>(services, configure);
+        }
+
+        public static IServiceCollection AddThreaxProcessHelper<TDefaultLogType>(this IServiceCollection services, Action<ThreaxProcessHelperOptions>? configure = null)
+        {
             var options = new ThreaxProcessHelperOptions();
             configure?.Invoke(options);
 
             services.TryAddScoped<IProcessRunner>(
-                s => CreateRunner(s, options));
+                s => CreateRunner<TDefaultLogType>(s, options));
 
             return services;
         }
 
-        private static IProcessRunner CreateRunner(IServiceProvider s, ThreaxProcessHelperOptions options)
+        private static IProcessRunner CreateRunner<TDefaultLogType>(IServiceProvider s, ThreaxProcessHelperOptions options)
         {
             IProcessRunner runner = new ProcessRunner();
             if (options.IncludeLogOutput)
             {
                 try
                 {
-                    var logger = s.GetRequiredService<ILogger<DefaultLog>>();
-                    runner = new LoggingProcessRunner<DefaultLog>(runner, logger);
+                    var logger = s.GetRequiredService<ILogger<TDefaultLogType>>();
+                    runner = new LoggingProcessRunner<TDefaultLogType>(runner, logger);
                 }
                 catch (ObjectDisposedException)
                 {
